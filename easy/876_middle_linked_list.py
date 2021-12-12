37eb514cd321ba75ee8000e797da290d566b10ce
krnr <ultrasnet@mail.ru>
2021-12-12 09:39:02 +0300

    remove Nth node from the end of the linked list

---
+++ easy/876_middle_linked_list.py
@@ -0,0 +1,43 @@
+"""
+Given the head of a singly linked list, return the middle node of the linked list.
+
+If there are two middle nodes, return the second middle node.
+
+Example 1:
+
+Input: head = [1,2,3,4,5]
+Output: [3,4,5]
+Explanation: The middle node of the list is node 3.
+
+Example 2:
+
+Input: head = [1,2,3,4,5,6]
+Output: [4,5,6]
+Explanation: Since the list has two middle nodes with values 3 and 4, we return the second one.
+
+Constraints:
+
+The number of nodes in the list is in the range [1, 100].
+1 <= Node.val <= 100
+"""
+from typing import *
+
+ListNode = TypeVar['ListNode']
+
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def middleNode(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        length = 0
+        middle = head
+        runner = head
+        while runner.next:
+            runner = runner.next
+            length += 1
+            if length % 2:
+                middle = middle.next
+        return middle
