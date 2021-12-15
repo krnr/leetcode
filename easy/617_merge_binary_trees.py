da26fc4bde2157497b8431e8027aa77e1804f41f
krnr <ultrasnet@mail.ru>
2021-12-15 11:26:48 +0300

    merge binary trees

--- easy/617_merge_binary_trees.py
+++ easy/617_merge_binary_trees.py
@@ -49,3 +49,16 @@ class Solution:
                 return root2
 
         return merge_node(root1, root2)
+
+
+class SolutionB:
+    def mergeTrees(
+        self, root1: Optional[TreeNode], root2: Optional[TreeNode]
+    ) -> Optional[TreeNode]:
+        if root1 and root2:
+            root1.val += root2.val
+            root1.left = self.mergeTrees(root1.left, root2.left)
+            root1.right = self.mergeTrees(root1.right, root2.right)
+            return root1
+        else:
+            return root1 or root2
